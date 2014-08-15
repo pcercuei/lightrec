9006b9abc29a6f4fb7fa10cfbb049ece2a71d19b
Paul Cercueil <paul@crapouillou.net>
2014-08-15 03:06:23 +0200

    emitter: Added example recompiling function rec_special_ADDU

--- emitter.c
+++ emitter.c
@@ -13,10 +13,34 @@
  */
 
 #include "emitter.h"
+#include "regcache.h"
 
 #include <lightning.h>
 
 void emit_call_to_interpreter(jit_state_t *_jit, union opcode op)
 {
 	/* TODO: Generate something... */
+}
+
+void rec_special_ADDU(jit_state_t *_jit, union opcode op)
+{
+	u8 rs, rt, rd;
+
+	jit_name(__func__);
+
+	rs = lightrec_alloc_reg_in(_jit, op.r.rs);
+	rt = lightrec_alloc_reg_in(_jit, op.r.rt);
+	rd = lightrec_alloc_reg_out(_jit, op.r.rd);
+
+	/* Propagate constants */
+	lightrec_rvals[rd].known = lightrec_rvals[rs].known
+		&& lightrec_rvals[rt].known;
+	if (lightrec_rvals[rd].known)
+		lightrec_rvals[rd].value = lightrec_rvals[rs].value +
+			lightrec_rvals[rd].value;
+
+	jit_note(__FILE__, __LINE__);
+	jit_addr(rd, rs, rt);
+
+	lightrec_free_regs();
 }
