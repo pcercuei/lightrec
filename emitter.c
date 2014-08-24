d827b7086f6917a056d5ed46e5b4d5bf98a2b139
Paul Cercueil <paul@crapouillou.net>
2014-08-24 17:40:39 +0200

    emitter: Add functions rec_J and rec_JR
    Currently, those functions will only generate a jump to the end of the
    wrapper block, to end the lightrec_execute_block() function.
    
    In the future, they should recompile the code at the target PC if
    needed, and jump to the new block directly.

--- emitter.c
+++ emitter.c
@@ -22,6 +22,43 @@ void emit_call_to_interpreter(jit_state_t *_jit, union opcode op)
 	/* TODO: Generate something... */
 }
 
+static uintptr_t __get_jump_address_cb(u32 pc)
+{
+	/* TODO: Recompile the block located at pc, and return its address */
+	return end_of_block;
+}
+
+static void lightrec_emit_end_of_block(jit_state_t *_jit,
+		u8 reg_new_pc, u32 imm)
+{
+	jit_name(__func__);
+	lightrec_free_regs();
+	lightrec_storeback_all_regs(_jit);
+
+	jit_note(__FILE__, __LINE__);
+
+	/* FIXME: This may not work on all architectures */
+	if (!reg_new_pc)
+		jit_movi(JIT_RA0, imm);
+	else
+		jit_movr(JIT_RA0, reg_new_pc);
+
+	jit_calli(&__get_jump_address_cb);
+	jit_retval(JIT_R0);
+	jit_jmpr(JIT_R0);
+}
+
+void rec_special_JR(jit_state_t *_jit, union opcode op)
+{
+	u8 rs = lightrec_alloc_reg_in(_jit, op.r.rs);
+	lightrec_emit_end_of_block(_jit, rs, 0);
+}
+
+void rec_J(jit_state_t *_jit, union opcode op)
+{
+	lightrec_emit_end_of_block(_jit, 0, op.j.imm);
+}
+
 void rec_special_ADDU(jit_state_t *_jit, union opcode op)
 {
 	u8 rs, rt, rd;
