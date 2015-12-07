79f5893d3910246692c98d47f9fe896f0515e074
Paul Cercueil <paul@crapouillou.net>
2015-12-07 14:50:06 +0100

    Make likely/unlikely macros cast to bool
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- lightrec.h
+++ lightrec.h
@@ -34,8 +34,8 @@ typedef int16_t s16;
 typedef int8_t  s8;
 
 #ifdef __GNUC__
-#	define likely(x)       __builtin_expect((x),1)
-#	define unlikely(x)     __builtin_expect((x),0)
+#	define likely(x)       __builtin_expect(!!(x),1)
+#	define unlikely(x)     __builtin_expect(!!(x),0)
 #else
 #	define likely(x)       (x)
 #	define unlikely(x)     (x)
