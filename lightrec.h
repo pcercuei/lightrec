ef6db09ec220c8178214eeff74ad59431c28136a
Paul Cercueil <paul@crapouillou.net>
2022-02-17 11:55:59 +0000

    lightrec: Require new callback .enable_ram
    This callback will be called by Lightrec when the RAM needs to be
    enabled or disabled.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- lightrec.h
+++ lightrec.h
@@ -91,6 +91,7 @@ struct lightrec_cop_ops {
 struct lightrec_ops {
 	struct lightrec_cop_ops cop0_ops;
 	struct lightrec_cop_ops cop2_ops;
+	void (*enable_ram)(struct lightrec_state *state, _Bool enable);
 };
 
 struct lightrec_registers {
