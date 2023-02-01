pcercuei/lightrec

commit e8f4f29649c636d453dac4ffd778960a3b6a23af

emitter: Remove branch in rec_store_direct()
Improve a bit the code of rec_store_direct() by using a conditional move
instruction instead of a branch.

Because of how the code works, in the case where the target address is
not in RAM, the code LUT at offset 0x0 will be invalidated. This is
okay, because the code block at PC 0x0 shouldn't be executed that often.

Signed-off-by: Paul Cercueil <paul@crapouillou.net>

emitter.c
@@ -1350,7 +1350,7 @@ static void rec_store_direct(struct lightrec_cstate *cstate, const struct block 
 	struct regcache *reg_cache = cstate->reg_cache;
 	union code c = block->opcode_list[offset].c;
 	jit_state_t *_jit = block->_jit;
-	jit_node_t *to_not_ram, *to_end;
+	jit_node_t *to_end;
 	bool swc2 = c.i.op == OP_SWC2;
 	u8 tmp, tmp2, tmp3, rs, rt, in_reg = swc2 ? REG_CP2_TEMP : c.i.rt;
 
@@ -1371,10 +1371,11 @@ static void rec_store_direct(struct lightrec_cstate *cstate, const struct block 
 	lightrec_free_reg(reg_cache, rs);
 	tmp = lightrec_alloc_reg_temp(reg_cache, _jit);
 
-	to_not_ram = jit_bgti(tmp2, ram_size);
+	jit_lti_u(tmp, tmp2, ram_size);
+	jit_movnr(tmp, tmp2, tmp);
 
 	/* Compute the offset to the code LUT */
-	jit_andi(tmp, tmp2, (RAM_SIZE - 1) & ~3);
+	jit_andi(tmp, tmp, (RAM_SIZE - 1) & ~3);
 	if (!lut_is_32bit(state))
 		jit_lshi(tmp, tmp, 1);
 	jit_addr(tmp, LIGHTREC_REG_STATE, tmp);
@@ -1390,8 +1391,6 @@ static void rec_store_direct(struct lightrec_cstate *cstate, const struct block 
 
 		to_end = jit_b();
 	}
-
-	jit_patch(to_not_ram);
 
 	if (state->offset_ram || state->offset_scratch)
 		jit_movi(tmp, state->offset_scratch);

commit 1fe158c88f0913e72d96b5e2cec443bcf4836688

emitter: Fix rec_store_direct() with non-optimal memory maps
Since commit e8f4f29, when the offsets for the RAM and scratchpad were
different the generated code would segfault, as the store address was
not computed properly anymore.

Fixes: e8f4f29 ("emitter: Remove branch in rec_store_direct()")
Signed-off-by: Paul Cercueil <paul@crapouillou.net>

emitter.c
@@ -1337,9 +1337,10 @@ static void rec_store_direct(struct lightrec_cstate *cstate, const struct block 
 	struct regcache *reg_cache = cstate->reg_cache;
 	union code c = block->opcode_list[offset].c;
 	jit_state_t *_jit = block->_jit;
-	jit_node_t *to_end;
+	jit_node_t *to_not_ram, *to_end;
 	bool swc2 = c.i.op == OP_SWC2;
-	u8 tmp, tmp2, tmp3, rs, rt, in_reg = swc2 ? REG_CP2_TEMP : c.i.rt;
+	u8 tmp, tmp2, tmp3, masked_reg, rs, rt;
+	u8 in_reg = swc2 ? REG_CP2_TEMP : c.i.rt;
 
 	jit_note(__FILE__, __LINE__);
 
@@ -1358,14 +1359,20 @@ static void rec_store_direct(struct lightrec_cstate *cstate, const struct block 
 	lightrec_free_reg(reg_cache, rs);
 	tmp = lightrec_alloc_reg_temp(reg_cache, _jit);
 
-	jit_lti_u(tmp, tmp2, ram_size);
-	jit_movnr(tmp, tmp2, tmp);
+	if (state->offset_ram != state->offset_scratch) {
+		to_not_ram = jit_bgti(tmp2, ram_size);
+		masked_reg = tmp2;
+	} else {
+		jit_lti_u(tmp, tmp2, ram_size);
+		jit_movnr(tmp, tmp2, tmp);
+		masked_reg = tmp;
+	}
 
 	/* Compute the offset to the code LUT */
 	if (c.i.op == OP_SW)
-		jit_andi(tmp, tmp, RAM_SIZE - 1);
+		jit_andi(tmp, masked_reg, RAM_SIZE - 1);
 	else
-		jit_andi(tmp, tmp, (RAM_SIZE - 1) & ~3);
+		jit_andi(tmp, masked_reg, (RAM_SIZE - 1) & ~3);
 
 	if (!lut_is_32bit(state))
 		jit_lshi(tmp, tmp, 1);
@@ -1381,6 +1388,7 @@ static void rec_store_direct(struct lightrec_cstate *cstate, const struct block 
 		jit_movi(tmp, state->offset_ram);
 
 		to_end = jit_b();
+		jit_patch(to_not_ram);
 	}
 
 	if (state->offset_ram || state->offset_scratch)