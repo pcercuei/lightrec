pcercuei/lightrec

commit 0b9fd6b018d2abc0103e0e9beb3f5334478f8c72

optimizer: Add pass to tag load opcodes in delay slots
Add the new OPT_HANDLE_LOAD_DELAYS optimization. With this pass enabled,
the load opcodes located in delay slots of branches or jumps will be
properly handled. These cases are no more handled by the
OPT_DETECT_IMPOSSIBLE_BRANCHES optimization pass.

Signed-off-by: Paul Cercueil <paul@crapouillou.net>

optimizer.c
@@ -1208,6 +1208,9 @@ static int lightrec_switch_delay_slots(struct lightrec_state *state, struct bloc
 		if (op_flag_sync(next->flags))
 			continue;
 
+		if (op_flag_load_delay(next->flags) && opcode_is_load(next_op))
+			continue;
+
 		if (!lightrec_can_switch_delay_slot(list->c, next_op))
 			continue;
 
@@ -1231,15 +1234,13 @@ static int lightrec_detect_impossible_branches(struct lightrec_state *state,
 	struct opcode *op, *list = block->opcode_list, *next = &list[0];
 	unsigned int i;
 	int ret = 0;
-	s16 offset;
 
 	for (i = 0; i < block->nb_ops - 1; i++) {
 		op = next;
 		next = &list[i + 1];
 
 		if (!has_delay_slot(op->c) ||
-		    (!load_in_delay_slot(next->c) &&
-		     !has_delay_slot(next->c) &&
+		    (!has_delay_slot(next->c) &&
 		     !(next->i.op == OP_CP0 && next->r.rs == OP_CP0_RFE)))
 			continue;
 
@@ -1247,20 +1248,6 @@ static int lightrec_detect_impossible_branches(struct lightrec_state *state,
 			/* The delay slot is the exact same opcode as the branch
 			 * opcode: this is effectively a NOP */
 			next->c.opcode = 0;
-			continue;
-		}
-
-		offset = i + 1 + (s16)op->i.imm;
-		if (load_in_delay_slot(next->c) &&
-		    (offset >= 0 && offset < block->nb_ops) &&
-		    !opcode_reads_register(list[offset].c, next->c.i.rt)) {
-			/* The 'impossible' branch is a local branch - we can
-			 * verify here that the first opcode of the target does
-			 * not use the target register of the delay slot */
-
-			pr_debug("Branch at offset 0x%x has load delay slot, "
-				 "but is local and dest opcode does not read "
-				 "dest register\n", i << 2);
 			continue;
 		}
 
@@ -1298,8 +1285,42 @@ static bool is_local_branch(const struct block *block, unsigned int idx)
 	}
 }
 
+static int lightrec_handle_load_delays(struct lightrec_state *state,
+				       struct block *block)
+{
+	struct opcode *op, *list = block->opcode_list;
+	unsigned int i;
+
+	for (i = 0; i < block->nb_ops; i++) {
+		op = &list[i];
+
+		if (!opcode_is_load(op->c) || !op->c.i.rt)
+			continue;
+
+		if (!is_delay_slot(list, i)) {
+			/* Only handle load delays in delay slots.
+			 * PSX games never abused load delay slots otherwise. */
+			continue;
+		}
+
+		if (is_local_branch(block, i - 1)
+		    && !opcode_reads_register(list[i + (s16)op->c.i.imm].c, op->c.i.rt)) {
+			/* The target opcode of the branch is inside
+			 * the block, and it does not read the register
+			 * written to by the load opcode; we can ignore
+			 * the load delay. */
+			continue;
+		}
+
+		op->flags |= LIGHTREC_LOAD_DELAY;
+	}
+
+	return 0;
+}
+
 static int lightrec_local_branches(struct lightrec_state *state, struct block *block)
 {
+	const struct opcode *ds;
 	struct opcode *list;
 	unsigned int i;
 	s32 offset;
@@ -1313,6 +1334,12 @@ static int lightrec_local_branches(struct lightrec_state *state, struct block *b
 		offset = i + 1 + (s16)list->c.i.imm;
 
 		pr_debug("Found local branch to offset 0x%x\n", offset << 2);
+
+		ds = get_delay_slot(block->opcode_list, i);
+		if (op_flag_load_delay(ds->flags) && opcode_is_load(ds->c)) {
+			pr_debug("Branch delay slot has a load delay - skip\n");
+			continue;
+		}
 
 		if (should_emulate(&block->opcode_list[offset])) {
 			pr_debug("Branch target must be emulated - skip\n");
@@ -1431,7 +1458,7 @@ static int lightrec_early_unload(struct lightrec_state *state, struct block *blo
 	struct opcode *op;
 	s16 last_r[34], last_w[34], last_sync = 0, next_sync = 0;
 	u64 mask_r, mask_w, dirty = 0, loaded = 0;
-	u8 reg;
+	u8 reg, load_delay_reg = 0;
 
 	memset(last_r, 0xff, sizeof(last_r));
 	memset(last_w, 0xff, sizeof(last_w));
@@ -1451,6 +1478,13 @@ static int lightrec_early_unload(struct lightrec_state *state, struct block *blo
 
 	for (i = 0; i < block->nb_ops; i++) {
 		op = &block->opcode_list[i];
+
+		if (OPT_HANDLE_LOAD_DELAYS && load_delay_reg) {
+			/* Handle delayed register write from load opcodes in
+			 * delay slots */
+			last_w[load_delay_reg] = i;
+			load_delay_reg = 0;
+		}
 
 		if (op_flag_sync(op->flags) || should_emulate(op)) {
 			/* The next opcode has the SYNC flag set, or is a branch
@@ -1472,6 +1506,15 @@ static int lightrec_early_unload(struct lightrec_state *state, struct block *blo
 
 		mask_r = opcode_read_mask(op->c);
 		mask_w = opcode_write_mask(op->c);
+
+		if (op_flag_load_delay(op->flags) && opcode_is_load(op->c)) {
+			/* If we have a load opcode in a delay slot, its target
+			 * register is actually not written there but at a
+			 * later point, in the dispatcher. Prevent the algorithm
+			 * from discarding its previous value. */
+			load_delay_reg = op->c.i.rt;
+			mask_w &= ~BIT(op->c.i.rt);
+		}
 
 		for (reg = 0; reg < 34; reg++) {
 			if (mask_r & BIT(reg)) {
@@ -2067,6 +2110,7 @@ static int (*lightrec_optimizers[])(struct lightrec_state *state, struct block *
 	IF_OPT(OPT_REMOVE_DIV_BY_ZERO_SEQ, &lightrec_remove_div_by_zero_check_sequence),
 	IF_OPT(OPT_REPLACE_MEMSET, &lightrec_replace_memset),
 	IF_OPT(OPT_DETECT_IMPOSSIBLE_BRANCHES, &lightrec_detect_impossible_branches),
+	IF_OPT(OPT_HANDLE_LOAD_DELAYS, &lightrec_handle_load_delays),
 	IF_OPT(OPT_TRANSFORM_OPS, &lightrec_transform_branches),
 	IF_OPT(OPT_LOCAL_BRANCHES, &lightrec_local_branches),
 	IF_OPT(OPT_TRANSFORM_OPS, &lightrec_transform_ops),

CMakeLists.txt
@@ -66,6 +66,7 @@ endif (ENABLE_THREADED_COMPILER)
 option(OPT_REMOVE_DIV_BY_ZERO_SEQ "(optimization) Remove div-by-zero check sequence" ON)
 option(OPT_REPLACE_MEMSET "(optimization) Detect and replace memset with host variant" ON)
 option(OPT_DETECT_IMPOSSIBLE_BRANCHES "(optimization) Detect impossible branches" ON)
+option(OPT_HANDLE_LOAD_DELAYS "(optimization) Detect load delays" ON)
 option(OPT_TRANSFORM_OPS "(optimization) Transform opcodes" ON)
 option(OPT_LOCAL_BRANCHES "(optimization) Detect local branches" ON)
 option(OPT_SWITCH_DELAY_SLOTS "(optimization) Switch delay slots" ON)

commit 2ef6b289d4337899d5f0a19d0e33fcd2758520a9

optimizer: Fix branches with CP2 delay slots not detected as impossible
Commit 0b9fd6b ("optimizer: Add pass to tag load opcodes in delay
slots") accidentally caused CP2 opcodes in delay slots of branches not
resulting in the branch being marked as "impossible".

Update the code to restore proper functionality. This makes Tekken 2
work again.

Signed-off-by: Paul Cercueil <paul@crapouillou.net>

interpreter.c
@@ -187,7 +187,7 @@ static u32 int_delay_slot(struct interpreter *inter, u32 pc, bool branch)
 	 * interpreter in that case.
 	 * Same goes for when we have a branch in a delay slot of another
 	 * branch. */
-	load_in_ds = load_in_delay_slot(op->c);
+	load_in_ds = opcode_is_load(op->c) || opcode_is_mfc(op->c);
 	branch_in_ds = has_delay_slot(op->c);
 
 	if (branch) {

optimizer.c
@@ -339,6 +339,38 @@ static bool reg_is_read_or_written(const struct opcode *list,
 	return reg_is_read(list, a, b, reg) || reg_is_written(list, a, b, reg);
 }
 
+bool opcode_is_mfc(union code op)
+{
+	switch (op.i.op) {
+	case OP_CP0:
+		switch (op.r.rs) {
+		case OP_CP0_MFC0:
+		case OP_CP0_CFC0:
+			return true;
+		default:
+			break;
+		}
+
+		break;
+	case OP_CP2:
+		if (op.r.op == OP_CP2_BASIC) {
+			switch (op.r.rs) {
+			case OP_CP2_BASIC_MFC2:
+			case OP_CP2_BASIC_CFC2:
+				return true;
+			default:
+				break;
+			}
+		}
+
+		break;
+	default:
+		break;
+	}
+
+	return false;
+}
+
 bool opcode_is_load(union code op)
 {
 	switch (op.i.op) {
@@ -454,46 +486,6 @@ static bool is_nop(union code op)
 	default:
 		return false;
 	}
-}
-
-bool load_in_delay_slot(union code op)
-{
-	switch (op.i.op) {
-	case OP_CP0:
-		switch (op.r.rs) {
-		case OP_CP0_MFC0:
-		case OP_CP0_CFC0:
-			return true;
-		default:
-			break;
-		}
-
-		break;
-	case OP_CP2:
-		if (op.r.op == OP_CP2_BASIC) {
-			switch (op.r.rs) {
-			case OP_CP2_BASIC_MFC2:
-			case OP_CP2_BASIC_CFC2:
-				return true;
-			default:
-				break;
-			}
-		}
-
-		break;
-	case OP_LB:
-	case OP_LH:
-	case OP_LW:
-	case OP_LWL:
-	case OP_LWR:
-	case OP_LBU:
-	case OP_LHU:
-		return true;
-	default:
-		break;
-	}
-
-	return false;
 }
 
 static void lightrec_optimize_sll_sra(struct opcode *list, unsigned int offset,
@@ -1241,6 +1233,7 @@ static int lightrec_detect_impossible_branches(struct lightrec_state *state,
 
 		if (!has_delay_slot(op->c) ||
 		    (!has_delay_slot(next->c) &&
+		     !opcode_is_mfc(next->c) &&
 		     !(next->i.op == OP_CP0 && next->r.rs == OP_CP0_RFE)))
 			continue;
 

optimizer.h
@@ -16,7 +16,7 @@ __cnst _Bool opcode_writes_register(union code op, u8 reg);
 __cnst u64 opcode_write_mask(union code op);
 __cnst _Bool has_delay_slot(union code op);
 _Bool is_delay_slot(const struct opcode *list, unsigned int offset);
-__cnst _Bool load_in_delay_slot(union code op);
+__cnst _Bool opcode_is_mfc(union code op);
 __cnst _Bool opcode_is_load(union code op);
 __cnst _Bool opcode_is_io(union code op);
 __cnst _Bool is_unconditional_jump(union code c);