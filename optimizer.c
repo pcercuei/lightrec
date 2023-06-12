ab0e2523846d5e5032f9794f5615b9831fbc5307
Paul Cercueil <paul@crapouillou.net>
2023-06-12 18:06:49 +0200

    optimizer: Don't handle load delays for LWC2
    The LWC2 register does not write a general-purpose register, and
    therefore we don't have to handle load delays when it is found in a
    delay slot.
    
    Besides, the LWC2 opcode emitter uses the temporary register which is
    also used to store the temporary value of a delayed load, which would
    cause problems in games.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- optimizer.c
+++ optimizer.c
@@ -1288,7 +1288,7 @@ static int lightrec_handle_load_delays(struct lightrec_state *state,
 	for (i = 0; i < block->nb_ops; i++) {
 		op = &list[i];
 
-		if (!opcode_is_load(op->c) || !op->c.i.rt)
+		if (!opcode_is_load(op->c) || !op->c.i.rt || op->c.i.op == OP_LWC2)
 			continue;
 
 		if (!is_delay_slot(list, i)) {
