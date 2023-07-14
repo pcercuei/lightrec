1e354150cb6a1d001cd8cc1c4cc038b0d9425c7b
Paul Cercueil <paul@crapouillou.net>
2023-07-14 16:48:41 +0200

    optimizer: Fix LWL/LWR opcodes being incorrectly swapped
    Final Fantasy 8 has a code block with a LW followed by a LWL, which is
    standalone - not followed by a LWR.
    
    This triggered a false positive in the optimization pass that detects
    intricated load delay dependencies, and caused the two opcodes to be
    swapped.
    
    Fix this by filtering out all the cases where the second opcode is a LWL
    or LWR in the optimization pass.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- optimizer.c
+++ optimizer.c
@@ -1340,7 +1340,7 @@ static int lightrec_swap_load_delays(struct lightrec_state *state,
 		} else if (!in_ds && opcode_is_load(c) && c.i.op != OP_LWC2) {
 			next = block->opcode_list[i + 1].c;
 
-			if (c.i.op == OP_LWL && next.i.op == OP_LWR)
+			if (next.i.op == OP_LWL || next.i.op == OP_LWR)
 				continue;
 
 			if (opcode_reads_register(next, c.i.rt)
