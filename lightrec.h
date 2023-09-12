7e42a2f6c2affa0a2f3e633c715e43bbba46044d
Paul Cercueil <paul@crapouillou.net>
2023-09-13 00:03:15 +0200

    Revert "lightrec: Use ANSI C types for fixed-size typedefs"
    This reverts commit 5a5b4d2f87cf05b941a73aa158b0f42fc48bad9c.
    
    Using custom types in the public header causes all kind of problems in
    the software that uses Lightrec, so it's better to just revert this
    commit.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- lightrec.h
+++ lightrec.h
@@ -12,6 +12,7 @@ extern "C" {
 #endif
 
 #include <stddef.h>
+#include <stdint.h>
 
 #ifdef _WIN32
 #   ifdef lightrec_EXPORTS
@@ -42,15 +43,15 @@ extern "C" {
 #   endif
 #endif
 
-typedef unsigned long long u64;
-typedef unsigned int u32;
-typedef unsigned short u16;
-typedef unsigned char u8;
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t  u8;
 
-typedef signed long long s64;
-typedef signed int s32;
-typedef signed short s16;
-typedef signed char s8;
+typedef int64_t s64;
+typedef int32_t s32;
+typedef int16_t s16;
+typedef int8_t  s8;
 
 struct lightrec_state;
 struct lightrec_mem_map;
