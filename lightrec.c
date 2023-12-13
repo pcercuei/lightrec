42e192eeec97938eb89b33eb09482acc7be5ba2e
Paul Cercueil <paul@crapouillou.net>
2023-12-13 19:46:17 +0100

    lightrec: Only read block cache once when compiling block
    Reading it twice is a race vs. the main thread. The scenario is, when
    compiling block 1, covering blocks 2 and 3, those last two are marked
    as dead. Meanwhile, the main thread wants to execute block 2, sees that
    it is marked as dead, and will re-create a brand new block 2 at the same
    block LUT address. When the threaded compiler re-loads all the blocks to
    destroy them (with the reaper), it will then get a pointer to the new
    block 2, instead of a pointer to the old block 2.
    
    Address this issue by using a huge local array that will be filled with
    pointers to the blocks covered by the block being compiled; then the
    correct blocks will be destroyed even if their respective entries in the
    code LUT now point to freshly re-generated blocks.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- lightrec.c
+++ lightrec.c
@@ -1567,6 +1567,7 @@ static void lightrec_reap_opcode_list(struct lightrec_state *state, void *data)
 int lightrec_compile_block(struct lightrec_cstate *cstate,
 			   struct block *block)
 {
+	struct block *dead_blocks[ARRAY_SIZE(cstate->targets)];
 	u32 was_dead[ARRAY_SIZE(cstate->targets) / 8];
 	struct lightrec_state *state = cstate->state;
 	struct lightrec_branch_target *target;
@@ -1710,6 +1711,8 @@ int lightrec_compile_block(struct lightrec_cstate *cstate,
 				was_dead[i / 32] &= ~BIT(i % 32);
 		}
 
+		dead_blocks[i] = block2;
+
 		/* If block2 was pending for compilation, cancel it.
 		 * If it's being compiled right now, wait until it finishes. */
 		if (block2)
@@ -1728,8 +1731,12 @@ int lightrec_compile_block(struct lightrec_cstate *cstate,
 		offset = lut_offset(block->pc) + target->offset;
 		lut_write(state, offset, jit_address(target->label));
 
-		offset = block->pc + target->offset * sizeof(u32);
-		block2 = lightrec_find_block(state->block_cache, offset);
+		if (ENABLE_THREADED_COMPILER) {
+			block2 = dead_blocks[i];
+		} else {
+			offset = block->pc + target->offset * sizeof(u32);
+			block2 = lightrec_find_block(state->block_cache, offset);
+		}
 		if (block2) {
 			pr_debug("Reap block 0x%08x as it's covered by block "
 				 "0x%08x\n", block2->pc, block->pc);
