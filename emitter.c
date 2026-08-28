715de10439e7395b2d18374a7cf49caaa88b4527
Paul Cercueil <paul@crapouillou.net>
2026-08-28 22:57:51 +0200

    emitter: Support 'load delay' flag on MFC/CFC opcodes
    The MFC0/2 and CFC0/2 opcodes also work like memory loads, and the value
    read will be available only after the first opcode after the branch has
    been executed.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- emitter.c
+++ emitter.c
@@ -2148,12 +2148,14 @@ rec_mfc0(struct lightrec_cstate *state, const struct block *block, u16 offset)
 {
 	struct regcache *reg_cache = state->reg_cache;
 	union code c = block->opcode_list[offset].c;
+	u32 flags = block->opcode_list[offset].flags;
+	bool load_delay = op_flag_load_delay(flags) && !state->no_load_delay;
 	jit_state_t *_jit = block->_jit;
-	u8 rt;
+	u8 rt, reg = load_delay ? REG_TEMP : c.i.rt;
 
 	jit_note(__FILE__, __LINE__);
 
-	rt = lightrec_alloc_reg_out(reg_cache, _jit, c.i.rt, REG_EXT);
+	rt = lightrec_alloc_reg_out(reg_cache, _jit, reg, REG_EXT);
 
 	jit_ldxi_i(rt, LIGHTREC_REG_STATE, lightrec_offset(regs.cp0[c.r.rd]));
 
@@ -2404,8 +2406,11 @@ static void rec_cp2_basic_MFC2(struct lightrec_cstate *state,
 			       const struct block *block, u16 offset)
 {
 	const union code c = block->opcode_list[offset].c;
+	u32 flags = block->opcode_list[offset].flags;
+	bool load_delay = op_flag_load_delay(flags) && !state->no_load_delay;
+	u8 reg = load_delay ? REG_TEMP : c.i.rt;
 
-	rec_cp2_do_mfc2(state, block, offset, c.r.rd, c.r.rt);
+	rec_cp2_do_mfc2(state, block, offset, c.r.rd, reg);
 }
 
 static void rec_cp2_basic_CFC2(struct lightrec_cstate *state,
@@ -2413,8 +2418,10 @@ static void rec_cp2_basic_CFC2(struct lightrec_cstate *state,
 {
 	struct regcache *reg_cache = state->reg_cache;
 	const union code c = block->opcode_list[offset].c;
+	u32 flags = block->opcode_list[offset].flags;
+	bool load_delay = op_flag_load_delay(flags) && !state->no_load_delay;
 	jit_state_t *_jit = block->_jit;
-	u8 rt;
+	u8 rt, reg = load_delay ? REG_TEMP : c.i.rt;
 
 	_jit_name(block->_jit, __func__);
 
@@ -2432,11 +2439,11 @@ static void rec_cp2_basic_CFC2(struct lightrec_cstate *state,
 	case 27:
 	case 29:
 	case 30:
-		rt = lightrec_alloc_reg_out(reg_cache, _jit, c.r.rt, REG_EXT);
+		rt = lightrec_alloc_reg_out(reg_cache, _jit, reg, REG_EXT);
 		jit_ldxi_s(rt, LIGHTREC_REG_STATE, cp2c_s_offset(c.r.rd));
 		break;
 	default:
-		rt = lightrec_alloc_reg_out(reg_cache, _jit, c.r.rt, REG_ZEXT);
+		rt = lightrec_alloc_reg_out(reg_cache, _jit, reg, REG_ZEXT);
 		jit_ldxi_ui(rt, LIGHTREC_REG_STATE, cp2c_i_offset(c.r.rd));
 		break;
 	}
